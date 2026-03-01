b0299d9013cd11162d2612149a4402b926e18db7
shushovitJR <ranashushovit@gmail.com>
2026-03-01 14:18:49 +0545

    small changes

--- src/controllers/requestController.ts
+++ src/controllers/requestController.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from 'express';
 import db from '../config/db.js';
-import { AuthRequest } from '../middlewares/authMiddleware';
+import { AuthRequest } from '../middlewares/authMiddleware.js';
 
 const formatDate = (value: any) => {
     if (!value) return null;
